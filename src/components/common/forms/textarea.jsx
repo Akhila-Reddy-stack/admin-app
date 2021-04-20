f30d41d25fba0c45f56e7c92053f99e1156b8865
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-04-20 17:10:15 +0530

    Update textarea.jsx

--- src/components/common/forms/textarea.jsx
+++ src/components/common/forms/textarea.jsx
@@ -2,13 +2,14 @@ import classnames from 'classnames';
 import { asField } from 'informed';
 import React, { Fragment } from 'react';
 
-export const Textarea = asField(({ fieldState, fieldApi, ...props }) => {
+export const Textarea = asField(({ fieldState, faClass,fieldApi, ...props }) => {
     const { value } = fieldState;
     const { setValue, setTouched } = fieldApi;
     const { field, onChange, onBlur, initialValue, forwardedRef, ...rest } = props;
     return (
         <Fragment>
             <div className="form-group">
+            {faClass && <i className={faClass}></i>}
                 {props.label && <label htmlFor={field}>{props.label}</label>}
                 <textarea
                     {...rest}
