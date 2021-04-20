35a8624bca2463966f1932c7c18d291d9c82290e
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-04-20 16:29:31 +0530

    Update Routes.jsx

--- src/components/common/Routes.jsx
+++ src/components/common/Routes.jsx
@@ -12,28 +12,36 @@ import CurrentUserDetails from "../UserDetails/forms/CurrentUserDetails";
 import Inventry from "../Inventry/index";
 
 class Routes extends PureComponent {
+  constructor(props) {
+    super(props);
+  this.state = {
+
+  }
+}
+
   render() {
+    console.log(this.props)
     return (
       <Switch>
-        <Route path="/" exact component={DashboardPage} />
-        <Route path="/dashboard" exact component={DashboardPage} />
-        <Route path="/dashboard/list" exact component={DashboardList} />
-        <Route path="/inventry/:pageName" exact component={Inventry} />
-        <Route path="/userDetails" exact component={UserDetailsCard} />
-        <Route path="/userDetails/history" exact component={UserHistory} />
-        <Route path="/userDetails/check" exact component={ListCalculation} />
-        <Route path="/userDetails/summary" exact component={ListCalculation} />
+        {/* <Route path="/" exact component={DashboardPage} /> */}
+        {/* <Route path="/dashboard" exact component={DashboardPage} /> */}
+        <Route path="/dashboard/list" exact component={DashboardList}  props={this.props.props} />
+        <Route path="/inventry/:pageName" exact component={Inventry}   props={this.props.props.props}/>
+        <Route path="/userDetails" exact component={UserDetailsCard}  props={this.props} />
+        <Route path="/userDetails/history" exact component={UserHistory}   props={this.props}/>
+        <Route path="/userDetails/check" exact component={ListCalculation}  props={this.props} />
+        <Route path="/userDetails/summary" exact component={ListCalculation}   props={this.props}/>
         <Route
           path="/userDetails/checkhistory"
           exact
-          component={UserHistoryPay}
+          component={UserHistoryPay}  props={this.props}
         />
-        <Route path="/userDetails/pay" exact component={UserCheckItemsList} />
-        <Route path="/userDetails/activeUser" exact component={CurrentUser} />
+        <Route path="/userDetails/pay" exact component={UserCheckItemsList}  props={this.props} />
+        <Route path="/userDetails/activeUser" exact component={CurrentUser}   props={this.props}/>
         <Route
           path="/userDetails/activeUserDetails"
           exact
-          component={CurrentUserDetails}
+          component={CurrentUserDetails}  props={this.props}
         />
       </Switch>
     );
