18ac9c05bfcb6b052a1289f4a9d1af38a93779c3
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-04-20 16:30:21 +0530

    Update index.jsx

--- src/components/Inventry/index.jsx
+++ src/components/Inventry/index.jsx
@@ -2,39 +2,36 @@ import InventryList from './InventryList';
 import AddInventry from './forms/AddInventry';
 import React, { Fragment, PureComponent } from 'react';
 
-class Inventry extends PureComponent {
-  state = {
-    userRole: ''
+function Inventry(props) {
+  console.log('i am i coming ')
+  console.log(props)
+  const {
+    match: {
+      params: { pageName },
+    },
+  } = props
+  console.log(pageName)
+  console.log(props)
+  switch (pageName) {
+    case 'list':
+      return <InventryList {...props} />
+    case 'upload':
+      return (
+        <AddInventry
+          props={props}
+          formType={'add'}
+          history={props.history}
+        />
+      )
+    case 'edit':
+      return (
+        <AddInventry
+          editstate={props.location.editstate}
+          history={props.history}
+          formType={'edit'}
+        />
+      )
+    default:
+      return <InventryList />
   }
-
-  async componentDidMount() {
-    console.log(";;;;;;;;; props", this.props)
-  }
-
-
-  frameLoad = () => {
-    const { match: { params: { pageName } } } = this.props;
-    const { userRole } = this.state;
-    switch (pageName) {
-      case 'list':
-        return <InventryList props={this.props} />
-      case 'upload':
-        return <AddInventry props={this.props} userRole={userRole} />
-      case 'edit':
-        return <AddInventry props={this.props} userRole={userRole} />
-      default:
-        return <InventryList props={this.props} />
-    }
-  }
-
-  render() {
-    return (
-      <Fragment>
-        {this.frameLoad()}
-      </Fragment>
-    )
-  }
-
 }
-
-export default Inventry;
